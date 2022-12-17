1663646e36edeb9b3218facbbaf598e269692b6c
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-18 02:56:53 +0300

    Added task of finding an ever number

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+int rem = (a % 2);
+
+if (rem is 0)
+    WriteLine("Число является четным!");
+else
+    WriteLine($"Число не четное.");
