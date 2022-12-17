14f6be6ad55f678912e50f71db351fa42273b4ac
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-18 01:31:08 +0300

    Creating a task to find the maximum number of two

---
+++ Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console; 
+
+Clear();
+
+WriteLine("Введите два часла: ");
+int a = int.Parse(ReadLine());
+int b = int.Parse(ReadLine());
+
+if (a > b)
+    WriteLine($"Первое число ({a}) больше второго ({b})");
+else
+    WriteLine($"Второе число ({b}) больше первого ({a})"); 
