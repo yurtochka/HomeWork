d035736fded6170ee471d5ba1b16c4920d889dcc
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-18 01:48:08 +0300

    Task of searching the max number of three

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введите три числа: ");
+int a = Convert.ToInt32(ReadLine());
+int b = Convert.ToInt32(ReadLine());
+int c = Convert.ToInt32(ReadLine());
+
+if (a > b && a > c)
+    WriteLine($"MAX = {a}");
+else 
+    if (b > a && b > c)
+        WriteLine($"MAX = {b}");
+    else
+        if (c > a && c > b)
+            WriteLine($"MAX = {c}");
+        
