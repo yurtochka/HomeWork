0f42ec4a932bf309735e4ea490ff758adadb044f
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-19 01:23:45 +0300

    Added task of finding all even numbers

---
+++ Task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+int n = Convert.ToInt32(ReadLine());
+int count = 1;
+WriteLine(count); 
+
+while (count <= n)
+{
+    if (count % 2 is 0)
+    {
+        WriteLine(count);       
+    }
+    count+=1;
+}
