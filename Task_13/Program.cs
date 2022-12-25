6d40c26d1262d67ddea2cdb4cce9d05b22628804
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-25 19:32:08 +0300

    Added task that searches for the third digit of a number

---
+++ Task_13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число: ");
+int n = Convert.ToInt32(ReadLine());
+
+int Count = (int)Math.Log10(n) + 1;
+int x = 0;
+int a = n;
+
+if (Count > 2)
+{
+    while (Count > 2)
+    {
+        x = a % 10;
+        a /= 10;
+        Count--;
+    }
+    WriteLine($"Третья цифра числа {n} -> {x}");
+}
+else
+{
+    WriteLine("В числе нет третьей цифры.");
+}
