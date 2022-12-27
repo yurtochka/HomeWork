d192fdb9bfc2d6236459bdab9370057546a4ad38
Kopyl Tatyana <yurtochka@yandex.ru>
2022-12-27 21:33:45 +0300

    Cube table task

---
+++ Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+using static System.Console;
+Clear(); 
+
+WriteLine("Введите число: ");
+int.TryParse(ReadLine(), out int n); 
+
+while (n > 0)
+{
+    WriteLine(Math.Pow(n, 3));
+    n--;
+}
