4828b600229e623bd7ce7c62647c06bb48fc9444
Kopyl Tatyana <yurtochka@yandex.ru>
2023-01-17 00:46:51 +0300

    Task sum digits in the number

---
+++ Task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static System.Console;
+Clear();
+
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = SumNumber(number);
+WriteLine(sum);
+
+
+
+
+int SumNumber(int A)
+{
+    int result = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        result = result + (A % 10);
+        A = A / 10;
+    }
+    return result;
+}
