c6415e86a14fc666106b261959f6eb2823ee2100
Kopyl Tatyana <yurtochka@yandex.ru>
2023-02-06 18:12:41 +0300

    Task the recursion method. Output all natural numbers from N to 1

---
+++ Task_64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая реализует рекурсивный метод вывода всех натуральных чисел в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+using static System.Console;
+Clear();
+
+WriteLine("Enter N: ");
+int N = int.Parse(ReadLine()!);
+
+WriteLine();
+WriteLine($"N = {N} -> {ArrayNatNum(N)}");
+
+string ArrayNatNum(int n)
+{
+    if (n == 1)
+        return n.ToString();
+    else
+        return $"{n}, {ArrayNatNum(n - 1)}";        
+}
