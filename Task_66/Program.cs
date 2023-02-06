c567657147de7447173641d829e37f73b7855a85
Kopyl Tatyana <yurtochka@yandex.ru>
2023-02-06 18:16:17 +0300

    Task the recursion method. Search sum the natural elements from M to N

---
+++ Task_66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая реализует рекурсивный метод нахождения суммы натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+using static System.Console;
+Clear();
+
+WriteLine("Enter values M and N to space: ");
+string[] val = ReadLine()!.Split(" ");
+int m = int.Parse(val[0]);
+int n = int.Parse(val[1]);
+
+int res = PrintNumbers(m, n);
+WriteLine();
+WriteLine($"M = {m}; N = {n} -> {res}");
+
+int PrintNumbers(int M, int N)
+{
+    return N == M ? N : M + PrintNumbers(M + 1, N);
+}
+
